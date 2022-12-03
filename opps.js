f8864612acadfef310875ae77371069b714bf9fd
Muskan-coder <83203071+MuskanValmiki@users.noreply.github.com>
2022-12-03 11:47:35 +0530

    Add files via upload

---
+++ opps.js
@@ -0,0 +1,52 @@
+// class parentsClass{
+//    constructor (color){
+//             this.color="blue"
+//    }
+// }
+
+// class childClass extends parentsClass{
+//     constructor(game){
+//         super()
+//         this.color="pink"
+//         this.game="badminton"
+//     }
+// }
+// person1=new childClass
+// console.log(person1)
+
+// class Stack{
+//   constructor(){
+//     this.stack=[];
+//   }
+//   addElement(element){
+//     this.stack.push(element);
+//   }
+//   removeElement(){
+//     this.stack.pop()
+//   }
+// }
+// let stack=new Stack
+// stack.addElement(10)
+// stack.removeElement()
+// console.log(stack)
+
+// class que{
+//     constructor(){
+//         this.que=[]
+//     }
+//     addElement(element){
+//         this.que.unshift(element)
+//     }
+//     removeElement(){
+//         this.que.shift()
+//     }
+// }
+// let data=new que
+// data.addElement(25)
+// data.removeElement()
+// console.log(data)
+
+
+//map will iterate all the element of array but filter is
+//fillering the element according condition and return new array.
+// reduce gave single output accordin to condition.
